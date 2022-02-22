467857bbb1081e52310632e3cc462c5897588353
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-02-22 16:30:50 -0300

    Finalizando app cara ou coroa

--- app/src/main/java/com/limadruga/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/limadruga/caraoucoroa/ResultadoActivity.java
@@ -2,13 +2,41 @@ package com.limadruga.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imageResultado;
+    private ImageButton buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imageResultado = findViewById(R.id.imageResultado);
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
+        //recuperar dados
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("número");
+
+        if(numero == 0){//cara
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+
+        }else{//coroa
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
